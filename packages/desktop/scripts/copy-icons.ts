9a177f905ced46a72f52e2ce1bb73a22e0dcfe64
wkgcass <wkgcass@hotmail.com>
2026-07-27 11:52:10 +0800

    fix(desktop): use prod icons in dev channel builds

--- packages/desktop/scripts/copy-icons.ts
+++ packages/desktop/scripts/copy-icons.ts
@@ -4,9 +4,9 @@ import { resolveChannel } from "./utils"
 const arg = process.argv[2]
 const channel = arg === "dev" || arg === "beta" || arg === "prod" ? arg : resolveChannel()
 
-const src = `./icons/${channel}`
+const src = `./icons/prod`
 const dest = "resources/icons"
 
 await $`rm -rf ${dest}`
 await $`cp -R ${src} ${dest}`
-console.log(`Copied ${channel} icons from ${src} to ${dest}`)
+console.log(`Copied prod icons from ${src} to ${dest} (channel: ${channel})`)
